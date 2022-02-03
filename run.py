581b0e53fa04812e515cf4ae337e3ba4522561d4
oyfml <ongyifan@live.com>
2022-02-04 01:21:06 +0800

    Add default value for Skip function

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel as PydanticBaseModel
+from pydantic.main import ModelMetaclass
+
 import inspect
-from typing import Any, Optional, List
-from copy import deepcopy
+from typing import Any, Optional, List, _GenericAlias
+from enum import Enum, EnumMeta
+
 import ast
 import textwrap
 import pytest
@@ -13,7 +16,6 @@ class BaseModel(PydanticBaseModel):
         validate_all = True
         extra = "ignore"
         use_enum_values = True
-    
     def __hash__(self):
         """
         Allows subclasses of base model to be hashable
@@ -22,18 +24,23 @@ class BaseModel(PydanticBaseModel):
         """
         return hash(repr((type(self),) + tuple(self.__dict__.values())))
 
-def Skip(_type: Any):
+def Skip(_type: Any, default=None):
     # does nothing but annotates
-    return _type
+    if isinstance(_type,
+        (
+            ModelMetaclass,
+            type,
+            _GenericAlias,
+            EnumMeta
+        )
+    ):
+        return _type
+    else:
+        raise ValueError("argument must be valid type")
 
 class AdvancedBaseModel(BaseModel):
-
     def __init__(self, *args: Any, **kwargs: Any):
-        super().__init__( *args, **kwargs)
-        # annotations = deepcopy(self.__annotations__)
-        # fields = deepcopy(self.__fields__)
-        # fields_set = deepcopy(self.__fields_set__)
-
+        super().__init__( *args, **kwargs)        
         # parse class definition's abstract syntax tree after removing indentation
         cls_def: ast.ClassDef = ast.parse(textwrap.dedent(inspect.getsource(self.__class__))).body[0]
         # get all assignments with type annotation under class; i.e. x: int or y: str = 1
@@ -45,6 +52,18 @@ class AdvancedBaseModel(BaseModel):
                 field_name_obj: ast.Name = ann_assign.target
                 if func_name_obj.id == "Skip":
                     field_name = field_name_obj.id
+                    # overwrite with default value
+                    if field_name not in self.__fields_set__:
+                        if (
+                            len(ann_assign.annotation.keywords) > 0
+                        ):
+                            keyword = [kw for kw in ann_assign.annotation.keywords if kw.arg == "default"][0]
+                            self.__dict__[field_name] = keyword.value.value
+                        elif (
+                            len(ann_assign.annotation.args) == 2
+                        ):
+                            constant = ann_assign.annotation.args[-1]
+                            self.__dict__[field_name] = constant.value
                     # remove key field if optional and unset
                     if (
                         not self.__fields__[field_name].required
@@ -53,44 +72,27 @@ class AdvancedBaseModel(BaseModel):
                         self.__delattr__(field_name)
 
 
-        # for field_repr in inspect.getsource(self.__class__).split("\n")[1:]:
-        #     if "# skip" in field_repr.lower():
-        #         field_name = field_repr.split(":")[0].strip()
-        #         if not fields[field_name].required and self.__dict__[field_name] is None:
-        #             self.__delattr__(field_name)
-        #             annotations.pop(field_name)
-        #             fields.pop(field_name)
-        #             if field_name in fields_set:
-        #                 fields_set.remove(field_name)
-
-        # object.__setattr__(self, "__annotations__", annotations)
-        # object.__setattr__(self, "__fields__", fields)
-        # object.__setattr__(self, "__fields_set__", fields_set)
-    
-    # def dict(self, *args: Any, **kwargs: Any):
-    #     return self.dict(*args, **kwargs, exclude_unset=True)
-
 def test_outer_skip():
     # Outer model is modified 
     class Inner(BaseModel):
         e: int
         f: Optional[int]
-        g: Skip(Optional[List[str]]) # does nothing
+        g: Skip(Optional[str]) # does nothing
 
     class Outer(AdvancedBaseModel):
         a: str
         b: Optional[str]
-        c: Skip(Optional[List[str]]) # skip
+        c: Skip(Optional[str]) # skip
         d: Inner
 
     t = Outer(a=1, d=Inner(e=1,f=2))
     t1 = Outer(a=1, c=None, d=Inner(e=1,f=2))
-    t2 = Outer(a=1, c=[2], d=Inner(e=1,f=2))
+    t2 = Outer(a=1, c=2, d=Inner(e=1,f=2))
 
     # skip c if assigned as None
     assert t.dict() == {'a': '1', 'b': None, 'd': {'e': 1, 'f': 2, 'g': None}}
     assert t1.dict() == {'a': '1', 'b': None, 'd': {'e': 1, 'f': 2, 'g': None}}
-    assert t2.dict() == {'a': '1', 'b': None, 'c': ['2'], 'd': {'e': 1, 'f': 2, 'g': None}}
+    assert t2.dict() == {'a': '1', 'b': None, 'c': '2', 'd': {'e': 1, 'f': 2, 'g': None}}
 
 
 def test_inner_skip():
@@ -114,6 +116,10 @@ def test_inner_skip():
     assert t2.dict() == {'a': '1', 'b': None, 'c': ['2'], 'd': {'e': 1, 'f': 2}}
   
 def test_both_skip():
+    class TestEnum(Enum):
+        ONE = "1"
+        TWO = "2"
+    
     # Both models are modified 
     class Inner(AdvancedBaseModel):
         e: int
@@ -123,17 +129,17 @@ def test_both_skip():
     class Outer(AdvancedBaseModel):
         a: str
         b: Optional[str]
-        c: Skip(Optional[List[str]]) # skip
+        c: Skip(Optional[TestEnum]) # skip
         d: Inner
 
     t = Outer(a=1, d=Inner(e=1,f=2))
     t1 = Outer(a=1, c=None, d=Inner(e=1,f=2))
-    t2 = Outer(a=1, c=[2], d=Inner(e=1,f=2))
+    t2 = Outer(a=1, c='2', d=Inner(e=1,f=2))
 
     # skip c and g if assigned as None
     assert t.dict() == {'a': '1', 'b': None, 'd': {'e': 1, 'f': 2}}
     assert t1.dict() == {'a': '1', 'b': None, 'd': {'e': 1, 'f': 2}}
-    assert t2.dict() == {'a': '1', 'b': None, 'c': ['2'], 'd': {'e': 1, 'f': 2}}
+    assert t2.dict() == {'a': '1', 'b': None, 'c': '2', 'd': {'e': 1, 'f': 2}}
 
 
 def test_nested_skip():
@@ -156,6 +162,19 @@ def test_nested_skip():
     assert t2.dict() == {'a': '1', 'b': None, 'c': {'e': 1, 'f': 2, 'g': None}}
 
 
+def test_assignment_skip():
+    class Test(AdvancedBaseModel):
+        a: str
+        b: Optional[str] = "string"
+        c: Skip(Optional[str], default="string2")
+        d: Skip(Optional[str], "string3")
+        e: Skip(Optional[str])
+
+    t = Test(a=1)
+    t1 = Test(a=1, b=None, c=None)
+    assert t.dict() == {'a': '1', 'b': "string", 'c': "string2", 'd': "string3"}
+    assert t1.dict() == {'a': '1', 'b': None, 'd': "string3"}
+
 def test_multi_line_skip():
     class A(AdvancedBaseModel):
         some_very_long_attribute_name_dont_ask_why: Skip(Optional[List[List[List[List[List[List[
@@ -169,3 +188,4 @@ def test_multi_line_skip():
     assert t.dict() == {}
     assert t1.dict() == {}
     assert t2.dict() == {'some_very_long_attribute_name_dont_ask_why': [[[[[["Test", "123"]]]]]]}
+
